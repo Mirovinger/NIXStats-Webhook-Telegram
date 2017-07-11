d843db69afaddb15c30d1274ba649527adc69af7
Evgen Lebed <ealebed@gmail.com>
2017-07-11 09:09:03 +0300

    Update sms.php

--- functions/sms.php
+++ functions/sms.php
@@ -1,10 +1,11 @@
 <?php
 function sms($message)
 {
+	global $turbosmslogin, $turbosmspassword, $turbosmssender, $turbosmsrecipient;
 	$client = new SoapClient ('http://turbosms.in.ua/api/wsdl.html');
-	$auth = array('login' => 'YOUR_LOGIN', 'password' => 'YOUR_VERY_STRONG_PASSWORD');
+	$auth = array('login' => $turbosmslogin, 'password' => $turbosmspassword);
 	$result = $client->Auth ($auth);
-	$sms = Array ('sender' => 'SENDER_NAME','destination' => '+380XXXXXXXXX,+380XXXXXXXXX','text' => $message);
+	$sms = Array ('sender' => $turbosmssender,'destination' => $turbosmsrecipient,'text' => $message);
 	$result = $client->SendSMS ($sms);
 	return $result;
 }
